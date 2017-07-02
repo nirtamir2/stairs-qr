2b68f194539bacb29adacc0bf4ff6e8b4ae5081f
Nir Tamir <nirtamir2@gmail.com>
2017-07-02 19:31:28 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-app>
+    <div id="app">
+      <video-bg :sources="['./static/video/stairsvideo.mp4']" img="./assets/logo.png">
+        <div id="toolbar-image">
+          <v-toolbar class="transparent elevation-0" light>
+            <v-toolbar-title class="hidden-sm-and-down">Toolbar</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-toolbar-items>
+              <v-toolbar-item v-for="item in items" :key="item">
+                {{ item.title }}
+              </v-toolbar-item>
+            </v-toolbar-items>
+          </v-toolbar>
+          <stairs-tab class="centered"></stairs-tab>
+
+        </div>
+      </video-bg>
+    </div>
+  </v-app>
+</template>
+
+<script>
+  import VideoBg from 'vue-videobg'
+  import StairsTab from './components/StairsTab.vue'
+  export default {
+    name: 'app',
+    components: {VideoBg, StairsTab},
+    data () {
+      return {
+        items: [
+          {title: 'About'},
+          {title: 'My Score'},
+          {title: 'High Scores'}
+        ]
+      }
+    }
+  }
+</script>
+
+<style>
+  #toolbar-image {
+    background: url('http://ww.powerpointhintergrund.com/uploads/2017/05/simple-backgrounds-wallpaper-2560x1440--47279-31.jpeg');
+  }
+
+  .centered {
+    position: fixed; /* or absolute */
+    top: 50%;
+    left: 50%;
+    transform: translateX(-50%) translateY(-50%);
+    width: 512px;
+  }
+
+</style>
---
+++ src/components/StairsTab.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-tabs id="mobile-tabs-3" grow light>
+    <v-tabs-bar slot="activators">
+      <v-tabs-slider></v-tabs-slider>
+      <v-tabs-item
+        v-for="header in headers"
+        :key="header"
+        :href="'#' + header"
+      >
+        header
+      </v-tabs-item>
+    </v-tabs-bar>
+    <v-tabs-content
+      v-for="header in headers"
+      :key="header"
+      :id=" '#'+ header"
+    >
+      <v-card flat>
+        <v-card-text>{{ text }}</v-card-text>
+      </v-card>
+    </v-tabs-content>
+  </v-tabs>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        headers: ['About', 'My Score', 'High Scores'],
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+      }
+    }
+  }
+</script>
